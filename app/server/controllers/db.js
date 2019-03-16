8b42bd154f8ecddfc4d40be0e2a1a5cefbfdea05
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-15 20:14:07 -0700

    Remove todo-app code from here

--- app/server/controllers/db.js
+++
@@ -1,6 +0,0 @@
-mongoose = require('mongoose')
-
-mongoose.connect(process.env['DB'])
-mongoose.connection.on('open', () => console.log('Connected to db'))
-
-module.exports = mongoose
--- go.sh
+++ go.sh
@@ -1,1 +1,4 @@
+git clone https://github.com/todo-deploy/todo-app.git
+mv todo-app/* .
+rm -rf todo-app/
 docker-compose up
